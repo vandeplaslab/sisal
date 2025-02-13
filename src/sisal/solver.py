ddf486b9075ecdddf972ccbd10b1d0e50f4de401
pauldelacour <paulod38@gmail.com>
2025-02-13 18:06:58 +0100

    *m

---
+++ src/sisal/solver.py
@@ -0,0 +1,258 @@
+import numpy as np
+from model import beta_vae,beta_vae_synthetic
+import torch
+import torch.nn.functional as F
+from torch.optim import Adam
+from torch.autograd import Variable 
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+#from metric import Metric
+from utils import reparametrize,compute_latent_mean,emp_std,limit_latent_space_precomp, metric_disentangling,compute_estimate_std
+import pickle as pkl
+
+
+#EPOCHS = 5
+#best_vloss = 1_000_000.
+
+
+# def reparametrize(mu, logvar):
+#         std = logvar.div(2).exp()
+#         eps = Variable(std.data.new(std.size()).normal_())
+#         return mu + std*eps
+
+class Solver():
+    def __init__(self,args, in_size) :
+        timestamp = datetime.now().strftime('%d%m%Y_%H%M%S')
+        self.writer = SummaryWriter('runs/fashion_trainer_{}'.format(timestamp))
+        self.device = torch.device(args.device)
+        self.z_dim = args.z_dim
+        self.model = beta_vae(args.z_dim,in_size).to(self.device)
+        self.save_epochs = args.save_model_epochs
+        #if args.synthetic : 
+        #    self.model = beta_vae_synthetic(args.z_dim).float()
+        self.EPOCHS = args.epochs
+        self.train_bool = args.train
+        self.PATH = 'model/model_weights.pth' ## Path to save the model
+        self.beta = args.beta
+        #self.metric = Metric()
+        self.evaluate_dis_metric = args.evaluate_dis_metric
+        #torch.save(self.model, self.PATH)
+        self.std_threshold = args.threshold_collapse
+        self.save_loss = args.save_loss
+        
+
+        
+
+    ## Average KL over the batch
+    def KL(self, mu, logvar ) : 
+        logvar = logvar.view(logvar.size(0), logvar.size(1))
+        #KL = 0.5*(mu.pow(2).sum() - logvar.sum() + logvar.exp().sum() - d*batch_size).div(batch_size)
+        klds = 0.5*(mu.pow(2)+logvar.exp()-logvar-1)
+        return klds.sum(1).mean(0)
+    
+    ##Average recons over the batch
+    def reconstruction_loss(self , x, mu_x): 
+        # - \E_{z \sim q_\phi(z|x)}(\log p_\theta(x|z))
+        #Model to start
+        #Param : q_\phi = N(0,diag(\sigma^2)), p_\theta(z) = N(0,I) , p_\theta(x|z) = N(\mu, I)
+        batch_size = x.size(0)
+       
+        r_loss = 0.5*F.mse_loss(x,mu_x, reduction='sum').div(batch_size)
+        return r_loss
+
+    def loss(self, beta,x, z_mean , z_logvar, decoder_mean):
+        return self.reconstruction_loss(x,decoder_mean) + beta*self.KL(z_mean,z_logvar)
+    
+    def train_one_epoch(self,epoch_index,dataloader,tb_writer):
+        c=0
+        optim = Adam(self.model.parameters(), lr=0.001, weight_decay=0.0001)
+        
+        running_loss = 0.   
+        last_loss = 0.  
+
+        running_recons = 0
+        running_KL = 0
+        for i,(x,_) in enumerate(dataloader) :
+                x = x.to(self.device, non_blocking=True)
+                z_mean,  z_logvar= self.model.forward(x) 
+                z = reparametrize(z_mean,z_logvar)
+
+                decoder_mean = self.model.decoder(z)
+                beta_vae_loss = self.loss(self.beta,x, z_mean,z_logvar,decoder_mean)
+                
+                optim.zero_grad()
+                beta_vae_loss.backward()
+                optim.step()
+                
+                running_loss += beta_vae_loss.item()
+                
+                ##############
+                reconstruction_loss = self.reconstruction_loss(x,decoder_mean)
+                running_recons += reconstruction_loss
+                kl_loss = self.KL(z_mean,z_logvar)
+                running_KL += kl_loss
+                ##############
+                
+                if i % 1000 == 999:
+                    last_loss = running_loss / 1000 # loss per batch
+                    print('  batch {} loss: {}'.format(i + 1, last_loss))
+                    tb_x = epoch_index * len(dataloader) + i + 1
+                    tb_writer.add_scalar('Loss/train', last_loss, tb_x)
+                    tb_writer.flush()
+                    running_loss = 0.
+
+                    ###############
+                    last_recons = running_recons/1000
+                    last_kl = running_KL/1000
+                    running_recons = 0
+                    running_KL = 0
+                    ###############
+                if i%2000 == 1999 :
+
+                    if self.save_epochs:
+                        torch.save(self.model, 'model/model_weight_n_{}.pth'.format(epoch_index*3+c))
+                        c+=1
+
+                    
+                    # if self.evaluate_dis_metric :
+                    #     with torch.no_grad():
+                    #         full_latent= compute_latent_mean(dataloader,self.model)
+                    #         full_std = emp_std(full_latent)
+                    #         z_min, z_max = limit_latent_space_precomp(full_latent)
+                    #         dis_metric = metric_disentangling(self.model,z_min,z_max,full_std)    
+                    #         tb_writer.add_scalar('Metric/disentangling', dis_metric, tb_x)
+                    #         tb_writer.add_scalar('Metric/reconstruction', running_recons, tb_x)
+                    # running_recons=0.
+        return last_loss , last_recons, last_kl
+
+    def evaluate_loss(self,loader) : 
+        with torch.no_grad():
+            running_loss = 0.
+            running_recons = 0
+            running_KL = 0
+            
+            for i,(x,_) in enumerate(loader) :
+                x = x.to(self.device, non_blocking=True)
+                z_mean,  z_logvar= self.model.forward(x) 
+                z = reparametrize(z_mean,z_logvar)
+                decoder_mean = self.model.decoder(z)
+                running_loss += self.loss(self.beta,x, z_mean,z_logvar,decoder_mean).item()
+                running_recons += self.reconstruction_loss(x,decoder_mean)
+                running_KL += self.KL(z_mean,z_logvar)
+                if i%1000 == 999 :
+                    return running_loss/1000 , running_recons/1000, running_KL/1000 
+
+
+    def train(self,train_loader, validation_loader, PATH , v=0):
+        best_vloss = 1e6
+        epoch_early_stop= 3 
+
+        if self.train_bool :
+            early_stop = 0 ## Counts the number of epochs without improvement 
+            results = []
+            if self.save_loss : 
+                train_loss , train_recons , train_kl, test_loss = self.evaluate_initialization(train_loader,validation_loader)
+                disentangling_metric = self.disentangling_metric_estimate(train_loader)
+                results.append([-1,train_loss,train_recons.item(),train_kl.item(),test_loss,disentangling_metric])
+            if self.save_epochs :
+                torch.save(self.model, 'model/model_weight_n_-1.pth')
+            #Training loop
+            for epoch in range(self.EPOCHS) :
+                if early_stop >= epoch_early_stop : 
+                    print('Early Stop, no improvements for {} epochs'.format(epoch_early_stop))
+                    if self.save_loss :
+                        print('#### saved_data/avg_models/model_z{}_b{}_v{}.npy'.format(self.z_dim,self.beta,v))
+                        with open('saved_data/avg_models/model_z{}_b{}_v{}.npy'.format(self.z_dim,self.beta,v), 'wb') as f:
+                            np.save(f, results)
+                    return PATH
+
+                
+                print('EPOCH {}:'.format(epoch + 1))
+                self.model.train(True)
+                avg_loss, avg_recons, avg_KL= self.train_one_epoch(epoch,train_loader,self.writer)
+                if self.save_epochs :
+                    torch.save(self.model, 'model/model_weight_n_{}.pth'.format(epoch*3+2))
+                # Evaluation on the validation set
+                self.model.train(False)
+                running_vloss = 0.0
+                with torch.no_grad():
+                    for x,_ in validation_loader :
+                        #x = x.to(self.device)
+                        x = x.to(self.device, non_blocking=True)
+                        z_mean,  z_logvar= self.model.forward(x) 
+                        z = reparametrize(z_mean,z_logvar)
+                        decoder_mean = self.model.decoder(z)
+                        beta_vae_loss = self.loss(self.beta, x, z_mean, z_logvar, decoder_mean)
+                        running_vloss += beta_vae_loss.item()
+
+                avg_vloss = running_vloss / len(validation_loader)
+
+                if self.save_loss :
+                    disentangling_metric = self.disentangling_metric_estimate(train_loader)
+                    results.append([epoch,avg_loss,avg_recons.item(),avg_KL.item(),avg_vloss,disentangling_metric])
+
+                print('LOSS train {} valid {}'.format(avg_loss,avg_vloss))
+                
+                if avg_vloss < best_vloss:
+                    early_stop = 0
+                    print('----------------Saving Model Epoch {}'.format(epoch+1))
+                    best_vloss = avg_vloss
+                    torch.save(self.model, PATH)
+                    # if self.save_epochs:
+                    #     torch.save(self.model, 'model/model_weight_epoch_{}.pth'.format(epoch+1))
+                
+                else :
+                    early_stop +=1
+
+                if self.evaluate_dis_metric :
+                        with torch.no_grad():
+                            full_latent= compute_latent_mean(train_loader,self.model)
+                            full_std = emp_std(full_latent)
+                            z_min, z_max = limit_latent_space_precomp(full_latent)
+                            dis_metric, _ = metric_disentangling(self.model,z_min,z_max,full_std,self.std_threshold)
+                            tb_x = epoch * len(train_loader)
+                            self.writer.add_scalar('Metric/disentangling', dis_metric, tb_x)
+                            #self.writer.add_scalar('Metric/reconstruction', running_recons, tb_x)                    
+
+            
+        if self.save_loss :
+            print('#### saved_data/avg_models/model_z{}_b{}_v{}.npy'.format(self.z_dim,self.beta,v))
+            with open('saved_data/avg_models/model_z{}_b{}_v{}.npy'.format(self.z_dim,self.beta,v), 'wb') as f:
+                print(results)
+                np.save(f, results)
+        return PATH   
+    
+    def evaluate_initialization(self,train_loader,test_loader):
+        with torch.no_grad():
+            train_loss, train_recons, train_kl = self.evaluate_loss(train_loader)
+            test_loss, _ , _ = self.evaluate_loss(test_loader)
+        return train_loss , train_recons , train_kl, test_loss
+
+    def disentangling_metric_estimate(self,train_loader):
+        n_b = 1
+        emp_std = compute_estimate_std(self.model,n_b,train_loader,self.device)
+        z_dim = self.model.z_dim
+        z_min = -3*np.ones(z_dim)
+        z_max = 3*np.ones(z_dim)
+        disentangling_metric,_ = metric_disentangling(self.model,z_min,z_max, emp_std.cpu() , self.std_threshold)
+        return disentangling_metric
+    
+
+def train_batch_models(args ,train_loader,test_loader):
+    args.train = True
+    args.z_dim = 10
+    n_rep = 4 #Number of times each model is computed
+    for r in range(n_rep) :
+        PATH = 'model/average_models/model_weights_z{}_b{}_v{}.pth'.format(args.z_dim,args.beta,r)
+        print(PATH)
+        net = Solver(args)
+        net.train(train_loader, test_loader,PATH,v=r)
+
+
+def train_batch_beta(args,in_size, train_loader, test_loader):
+    args.train = True
+    args.zdim = 2
+    PATH = 'model/average_models/mouse_pup/model_b_{}.pth'.format(args.beta)
+    print(PATH)
+    net = Solver(args,in_size)
+    net.train(train_loader,test_loader,PATH)
---
+++ src/sisal/utils.py
@@ -0,0 +1,331 @@
+import numpy as np
+import torch
+from collections import Counter
+from torch.autograd import Variable
+
+
+
+def reparametrize(mu, logvar):
+        std = logvar.div(2).exp()
+        eps = Variable(std.data.new(std.size()).normal_())
+        return mu + std*eps
+
+def sample(m, var):
+    return m + np.sqrt(var)*np.random.normal(size=m.shape[0])
+
+def sample_batch(m,var):
+    return m + np.sqrt(var)*np.random.normal(size=m.shape)
+
+# def sample(m, var):
+#     return m + np.sqrt(var)*np.random.normal(size=m.shape[0])
+
+## Compute the full latent space of the data given in loader 
+## Return the associate latent mean, variance and label
+def compute_latent(loader,model): 
+        print('Compute Latent') 
+        latent = np.zeros((len(loader.dataset),1+model.z_dim))  
+        vars = np.zeros((len(loader.dataset),model.z_dim))  
+        label = np.zeros(len(loader.dataset))
+        with torch.no_grad():
+            prev = 0
+            for x,l , i in loader :
+            #for i, (x,l)  in enumerate(loader) :
+                #x =x.to(device)
+                z_mean,  z_logvar =model.forward(x)
+                batch = z_mean.size(0)
+                latent[prev:prev+batch,0] = i
+                #print('# i = ', i)
+                latent[prev:prev+batch,1:] = z_mean.detach().numpy()
+                vars[prev:prev+batch,:] = np.exp(z_logvar.detach().numpy())
+                label[prev:prev+batch] = l
+                prev+=batch 
+                
+        
+        n_zeros_rows = ~np.all(latent == 0, axis=1)
+        latent = latent[n_zeros_rows]
+        vars = vars[n_zeros_rows]
+        label = label[n_zeros_rows]
+
+        with open('saved_data/saved_latent.npy', 'wb') as f:
+            np.save(f, latent)
+            np.save(f, vars)
+            np.save(f,label)
+        print('End Compute latent')
+
+        return latent, vars,label
+
+def compute_latent_synthetic(loader,model): 
+        print('Compute Latent') 
+        latent = np.zeros((len(loader.dataset),1+model.z_dim))  
+        vars = np.zeros((len(loader.dataset),model.z_dim))  
+        label = np.zeros(len(loader.dataset))
+        coeff = np.zeros(len(loader.dataset))
+        with torch.no_grad():
+            prev = 0
+            for x,l , alpha ,i in loader :
+            #for i, (x,l)  in enumerate(loader) :
+                #x =x.to(device)
+                z_mean,  z_logvar =model.forward(x)
+                batch = z_mean.size(0)
+                latent[prev:prev+batch,0] = i
+                #print('# i = ', i)
+                latent[prev:prev+batch,1:] = z_mean.detach().numpy()
+                vars[prev:prev+batch,:] = np.exp(z_logvar.detach().numpy())
+                label[prev:prev+batch] = l
+                coeff[prev:prev+batch] = alpha
+                prev+=batch 
+                
+        
+        with open('saved_data/saved_latent.npy', 'wb') as f:
+            np.save(f, latent)
+            np.save(f, vars)
+            np.save(f,label)
+        print('End Compute latent')
+        
+        n_zeros_rows = ~np.all(latent == 0, axis=1)
+        latent = latent[n_zeros_rows]
+        vars = vars[n_zeros_rows]
+        label = label[n_zeros_rows]
+
+        return latent, vars,label, coeff
+
+## Compute the mean of the latent only
+def compute_latent_mean(loader,model) :
+    print('Compute Latent mean')
+    latent = np.zeros((len(loader.dataset),model.z_dim))  
+    with torch.no_grad():
+        prev = 0
+        #for x,l , i in loader :    
+        for  x,_  in loader :
+            #x =x.to(device)
+            z_mean, _ = model.forward(x)
+            batch = z_mean.size(0)
+            #latent[prev:prev+batch,0] = i
+            latent[prev:prev+batch,:] = z_mean.detach().numpy()
+            prev+=batch 
+    print('End Compute latent mean')
+    return latent
+
+# n_b : number of batches on which to compute the estimate
+def compute_estimate_std(model,n_b,loader, device):
+    batch_size = 32
+    latent = np.zeros((n_b*batch_size,model.z_dim))  
+    with torch.no_grad():
+        prev = 0
+        #for x,l , i in loader :    
+        for _ in range(n_b):
+            x , _ = next(iter(loader))
+            x = x.to(device, non_blocking=True)
+            #x =x.to(device)
+            z_mean, _ = model.forward(x)
+            batch = z_mean.size(0)
+            emp_std = torch.std(z_mean,axis = 0, unbiased=True)
+            prev+=batch 
+    return emp_std
+
+
+
+# Compute the empirical standard distribution of the full data
+def emp_std(full_latent) : 
+    return np.std(full_latent,axis=0,ddof=1)    
+    #return np.sqrt(np.var(full_latent,axis = 0,ddof=1))
+
+# Compute the limit of the latent space using the full precomputed latent_space
+def limit_latent_space_precomp(full_latent):
+    z_min = np.min(full_latent, axis = 0)
+    z_max = np.max(full_latent, axis = 0)
+    return (z_min,z_max)
+
+def accuracy_confusion_matrix(confusion_matrix):
+        return np.sum(np.diag(confusion_matrix))/np.sum(confusion_matrix)
+
+
+def metric_disentangling(model, z_min,z_max, full_std , std_threshold):
+    #std_threshold = 0.2
+    L = 100
+    #L=32
+    M = 800 
+    #print('full_std =', full_std)
+    #print('len(full_std) = ', len(full_std))
+    z_dim = len(full_std)
+    
+    z_dims = np.arange(z_dim)
+    #active_dims = (full_std>=std_threshold).cpu()
+    active_dims = full_std>=std_threshold
+    if not(all(active_dims)):
+        print('Warning some dims have collapsed = ', z_dim-sum(active_dims))
+    
+    factors = np.random.choice(z_dims[active_dims], M) #Factors that are kept fixed
+    metric_data = np.zeros((M,2))
+    
+    #factors = np.random.choice(z_dim, M) #Factors that are kept fixed
+    with torch.no_grad():
+        for i in range(M) : 
+            f = factors[i]
+            random_factors = np.zeros((L,z_dim))
+            fixed_value = np.random.uniform(low = z_min[f], high = z_max[f])
+            random_factors[:,f] = fixed_value
+            not_f = ~np.isin(np.arange(z_dim),[f])
+            random_factors[:,not_f] = np.random.uniform(low = z_min[not_f], high = z_max[not_f], size = (L,z_dim-1))
+            random_factors = np.float32(random_factors)
+            ############
+            random_factors = torch.tensor(random_factors).to('cuda', non_blocking=True)
+            ############
+            #print('## Random_factors = ', random_factors)
+            #print(random_factors.shape)
+
+            random_data = model.decoder(random_factors)
+            
+            latent = model.encoder(random_data).cpu().detach().numpy()
+            mu = latent[:,:z_dim]
+            logvar = latent[:,z_dim:]
+            
+            random_latent = sample_batch(mu,np.exp(logvar))
+            
+            #random_latent = random_latent/full_std
+            random_latent = random_latent/full_std.detach().numpy()
+
+            # if i == 0 : 
+            #     print('##Factor = ' , f)
+            #     print('##Random Latent ')
+            #     print(random_latent[:5,:])
+            #print('## Random latent = ', random_latent.shape)
+            #print('#### full_std = ', full_std.shape)
+            random_latent = random_latent
+            emp_var = np.var(random_latent , axis=0,ddof=1) ## Unbiased estimate of the variance
+
+            metric_data[i,0] = np.argmin(emp_var)
+            metric_data[i,1] = f
+
+    less_var = {}
+    for f in range(z_dim):
+        c = Counter(metric_data[metric_data[:,0]==f,1])
+        pred = c.most_common(1)
+        less_var[f] = pred
+    
+    confusion_matrix = np.zeros((z_dim,z_dim))
+    for f in range(z_dim) : 
+        # Predicted factors when the true factor was f
+        bins = np.bincount(metric_data[metric_data[:,1]==f,0].astype(int))
+        for j in range(len(bins)):
+            confusion_matrix[f,j] = bins[j]
+    confusion_matrix = confusion_matrix[active_dims,:] 
+
+    accuracy = accuracy_confusion_matrix(confusion_matrix)
+    
+    #accuracy = accuracy_confusion_matrix(confusion_matrix)
+    # print('### Confusion Matrix')
+    # print(confusion_matrix)
+    # print('### Accuracy')
+    # print(accuracy)
+    return  accuracy , confusion_matrix 
+
+
+
+def accuracy_confusion_matrix(confusion_matrix) :
+    classifier = np.argmax(confusion_matrix, axis=1)
+    accuracy = np.sum(confusion_matrix[np.arange(confusion_matrix.shape[0]),classifier])*1.0/np.sum(confusion_matrix)
+    return accuracy
+def metric_disentangling_factorising(model,full_latent,vars,z_min,z_max):
+        print('## SECOND metric')
+        z_dim = len(z_min)
+        L = 100 #In the paper L = 100 
+        M = 800 #take the majority vote classifier from 800 votes
+        full_std = emp_std(full_latent[:,1:])
+        
+
+        index = np.random.choice(full_latent.shape[0],M, replace = False) 
+        #factors = np.random.binomial(1, 0.5, M)
+        factors = np.random.choice(z_dim, M) #Factors that are kept fixed
+        
+        metric_data = np.zeros((M,2)) #In first pos is that majority variance and in 1 is the label factor
+        #for p,i in enumerate(index):
+        for p in range(M):
+            f = factors[p]
+            #z = sample(full_latent[i,1:],vars[i])
+            #z=full_latent[i,1:]
+            
+            random_factors = np.zeros((L,z_dim))
+            #random_fac tors[:,f] = z[f] + np.random.normal(scale = 10)
+            fixed_value = np.random.uniform(low = z_min[f], high = z_max[f])
+            random_factors[:,f] = fixed_value
+            not_f = ~np.isin(np.arange(z_dim), [f])            
+            random_factors[:,not_f] = np.random.uniform(low = z_min[not_f] , high = z_max[not_f] ,size = (L,z_dim-1))            
+            
+            random_data = model.decoder(torch.Tensor(random_factors))
+            random_latent = model.encoder(random_data).detach().numpy()[:,:z_dim]
+            
+            #random_latent = random_latent.detach().numpy()[:,:z_dim]
+            random_latent = random_latent/full_std
+
+            emp_var = np.var(random_latent , axis=0,ddof=1)
+            
+            metric_data[p,0] = np.argmin(emp_var)
+            metric_data[p,1] = f
+        
+        less_var = {}
+        
+        for f in range(z_dim):
+            c = Counter(metric_data[metric_data[:,0]==f,1])
+            pred = c.most_common(1)
+            less_var[f] = pred
+        
+        confusion_matrix = np.zeros((z_dim,z_dim))
+
+        for f in range(z_dim) : 
+            # Predicted factors when the true factor was f
+            bins = np.bincount(metric_data[metric_data[:,1]==f,0].astype(int))
+            for j in range(len(bins)):
+                confusion_matrix[f,j] = bins[j]
+        
+        classifier = np.argmax(confusion_matrix, axis=1)
+        accuracy = np.sum(confusion_matrix[np.arange(confusion_matrix.shape[0]),classifier])*1.0/np.sum(confusion_matrix)
+        #accuracy = accuracy_confusion_matrix(confusion_matrix)
+
+        print('##Confusion Matrix')
+        print(confusion_matrix)
+        print('##Accuracy = ', accuracy)
+
+        return  confusion_matrix
+
+## load a lot of models and take their accuracy, ...
+# n = number of versions to load
+# b = value of beta
+# z_dim dim of latent space 
+def load_results(b,n,z_dim):
+    #z_dim = 10
+    results = []
+    for v in range(n):
+        with open('saved_data/avg_models/model_z{}_b{}_v{}.npy'.format(z_dim,b,v), 'rb') as f:
+            results.append(np.load(f))
+    return results
+
+def compute_loss(b,  n=1):
+    names = ['epoch' ,'train_loss', 'train_recons', 'train_kl', 'test_loss', 'dis_metric']
+    names_to_pos = dict(zip(names, range(len(names))))
+
+    
+    z_dim = 10
+    results = load_results(b,n,z_dim)
+
+    epochs = len(results[0])
+    train_loss = np.zeros((n,epochs))
+    train_recons = np.zeros((n,epochs))
+    train_kl = np.zeros((n,epochs))
+    test_loss = np.zeros((n,epochs))
+    dis_metric = np.zeros((n,epochs))
+    for i in range(n):
+        train_loss[i,:] = np.array(results[i])[:,names_to_pos['train_loss']]
+        train_recons[i,:] = np.array(results[i])[:,names_to_pos['train_recons']]
+        train_kl[i,:] = np.array(results[i])[:,names_to_pos['train_kl']]
+        test_loss[i,:] = np.array(results[i])[:,names_to_pos['test_loss']]
+        dis_metric[i,:] = np.array(results[i])[:,names_to_pos['dis_metric']]
+
+    return train_loss,train_recons,train_kl , test_loss,dis_metric
+
+
+
+
+
+
+
