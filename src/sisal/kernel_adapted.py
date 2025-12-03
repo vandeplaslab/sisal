7e26bb5fbbc911f2c43a3a2c7caa6ea2e8432dad
Lukasz Migas <lukas.migas@yahoo.com>
2025-12-03 15:52:50 +0000

    Update kernel_adapted.py

--- src/sisal/kernel_adapted.py
+++ src/sisal/kernel_adapted.py
@@ -3,6 +3,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+try:
+    from numba import njit, prange
+except ImportError:
+    def njit(*args, **kwargs):
+        def wrapper(func):
+            return func
+        return wrapper
+
+    def prange(*args, **kwargs):
+        return range(*args, **kwargs)
 
 def kernel_adapated(full_latent: np.ndarray, vars: np.ndarray) -> np.ndarray:
     X, Y = np.mgrid[-4:4:200j, -4:4:200j]
@@ -14,7 +24,6 @@ def kernel_adapated(full_latent: np.ndarray, vars: np.ndarray) -> np.ndarray:
 
     image = np.zeros(len(grid[0]))
 
-    n = len(full_latent)
     for i in range(n):
         image = image + np.exp(
             -0.5
@@ -27,6 +36,58 @@ def kernel_adapated(full_latent: np.ndarray, vars: np.ndarray) -> np.ndarray:
     image_r = image.reshape((im_size, im_size), order="C")
     image_r = np.rot90(image_r)
     return image_r
+
+def kernel_adapted_opt(full_latent: np.ndarray, vars: np.ndarray) -> np.ndarray:
+    X, Y = np.mgrid[-4:4:200j, -4:4:200j]
+    im_size = X.shape[0]
+    
+    grid_x = X.ravel()
+    grid_y = Y.ravel()
+
+    n = len(full_latent)
+    h = 4 * n ** (-1 / 6)
+
+    # We pass the flattened arrays to the JIT function
+    image = _compute_density_corrected(
+        grid_x, grid_y, 
+        full_latent[:, 1], full_latent[:, 2], 
+        vars[:, 0], vars[:, 1], 
+        h, n
+    )
+
+    image = image / (2 * np.pi * n * h**2)
+    image_r = image.reshape((im_size, im_size), order="C")
+    image_r = np.rot90(image_r)
+    
+    return image_r
+
+@njit(parallel=True, fastmath=True)
+def _compute_density_corrected(grid_x: np.ndarray, grid_y: np.ndarray, latents_1: np.ndarray, latents_2: np.ndarray, vars_0: np.ndarray, vars_1: np.ndarray, h: float, n: int) -> np.ndarray:
+    n_pixels = grid_x.shape[0]
+    image = np.zeros(n_pixels)
+    
+    # Pre-calculate the log part.
+    log_terms = 0.5 * (np.log(vars_0) + np.log(vars_1))
+    
+    # Pre-calculate constants
+    h_sq = h**2
+    
+    for p in prange(n_pixels):
+        gx = grid_x[p]
+        gy = grid_y[p]
+        sum_val = 0.0
+        
+        for i in range(n):
+            # Compute Distance Squared
+            dist_sq = (gx - latents_1[i]) ** 2 / vars_0[i] + \
+                      (gy - latents_2[i]) ** 2 / vars_1[i]
+            # Apply -0.5 and divide by h^2 (First Term)
+            term_1 = -0.5 * dist_sq / h_sq
+            # Subtract the log term (Second Term)
+            exponent = term_1 - log_terms[i]
+            sum_val += np.exp(exponent)
+        image[p] = sum_val
+    return image
 
 
 def plot_kernel_adapted(ax: plt.Axes, fig: plt.Figure, image: np.ndarray) -> None:
